89e603f06b65ba96ee0188b9b798d3bdf06cbf56
RiSh6 <rishabx000@gmail.com>
2023-03-05 23:58:31 +0100

    Test1

--- Assets/Scripts/Calculation.cs
+++ Assets/Scripts/Calculation.cs
@@ -7,12 +7,18 @@ public class Calculation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print(add(13, 2));
+        print("hey");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    int add(int fst, int snd)
+    {
+        return fst + snd;
     }
 }
